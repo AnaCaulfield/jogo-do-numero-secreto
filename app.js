52c5df0a5b5ef592205b53da4a8198a135dcffbf
Ana Caulfield <contato.anacaulfield@gmail.com>
2024-08-08 12:33:13 -0300

    de volta ao original

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 5;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
@@ -9,10 +9,9 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-  exibirTextoNaTela('h1', 'Clique para ser xingado');
+  exibirTextoNaTela('h1', 'Jogo do número secreto');
   exibirTextoNaTela(
-    'p',
-    'Escolha um número entre 1 e 5 seu doental do caralho'
+    'p','Escolha um número entre 1 e 100'
   );
 }
 
@@ -21,16 +20,16 @@ exibirMensagemInicial();
 function verificarChute() {
   let chute = document.querySelector('input').value;
   if (chute == numeroSecreto) {
-    exibirTextoNaTela('h1', 'Acertou Mizeravi!');
+    exibirTextoNaTela('h1', 'Acertou!!');
     let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-    let mensagemTentativas = `Você descobriu o número secreto seu lixo com ${tentativas} ${palavraTentativa}!`;
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
     exibirTextoNaTela('p', mensagemTentativas);
     document.getElementById('reiniciar').removeAttribute('disabled');
   } else {
     if (chute > numeroSecreto) {
-      exibirTextoNaTela('p', 'O número secreto é menor idiota');
+      exibirTextoNaTela('p', 'O número secreto é menor');
     } else {
-      exibirTextoNaTela('p', 'o número secreto é maior imbecil');
+      exibirTextoNaTela('p', 'o número secreto é maior');
     }
     tentativas++;
     limparCampo();
